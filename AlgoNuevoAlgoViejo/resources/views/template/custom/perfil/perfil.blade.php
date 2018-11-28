db10ab0035396e2fb6e09b25ab63f6baf4b44afb
NicoIglesias <nico.iglesias96@gmail.com>
2018-11-28 17:14:31 -0300

    carpetas y views creadas

---
+++ AlgoNuevoAlgoViejo/resources/views/template/custom/perfil/perfil.blade.php
@@ -0,0 +1,100 @@
+<style>
+
+  html,body {
+    background: #F8EEBC;
+    font-family: "Arial";
+  }
+
+  .container {
+    max-width: 1250px;
+    margin-top: 100px;
+    padding: 0;
+    width: 90%;
+    background-color: #FCE9E9;
+    box-shadow: 0 3px 6px rgba(0,0,0,0.10), 0 3px 6px rgba(0,0,0,0.10);
+  }
+
+  .h1{
+    text-align: center;
+    margin-top: 35px;
+    font-size: 40px;
+  }
+
+  main {
+    padding: 20px 20px 0px 20px;
+  }
+
+  .left {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .foto {
+    width: 200px;
+    height: 200px;
+    margin-top: -12px;
+
+  }
+
+  .nombre {
+    margin-top: 20px;
+    font-family: "Open Sans";
+    font-weight: 600;
+    font-size: 18pt;
+    color: #777;
+  }
+
+  .info {
+    margin-top: -5px;
+    margin-bottom: 5px;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 11pt;
+    color: #aaa;
+  }
+
+  .descripcion {
+    text-align: center;
+    margin-top: 25px;
+    margin: 25px 40px;
+    color: #999;
+    font-size: 11pt;
+    font-family: "Open Sans";
+    padding-bottom: 25px;
+    border-bottom: 1px solid #ededed;
+  }
+
+  .right {
+    padding: 0 25px 0 25px;
+  }
+  .galeria {
+    margin-top: 35px;
+  }
+
+  .galeria div {
+    margin-bottom: 30px;
+  }
+
+  .galeria img {
+    box-shadow: 0 3px 6px rgba(0,0,0,0.10), 0 3px 6px rgba(0,0,0,0.10);
+    width: auto;
+    height: auto;
+    cursor: pointer;
+    max-width: 100%;
+  }
+
+  .img{
+    padding: 95px;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+    text-align: center;
+    font-size: 20px;
+  }
+
+
+
+  }
+</style>
